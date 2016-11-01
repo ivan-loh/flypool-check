718cc3e775806484e8878868636d75755e9f105d
ivan <ivan@eriad.com>
2016-11-01 23:15:00 +0800

    initial version

---
+++ app.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const config  = require('config');
+const request = require('request');
+const exec    = require('child_process').exec;
+
+
+/**
+ * configs
+ **/
+
+const threshold = config.get('threshold');
+const command   = config.get('command');
+const url       = config.get('endpoint') + config.get('wallet');
+
+
+/**
+ * loooop
+ **/
+
+(function check() {
+    
+    request(url, (err, response, body) => {
+
+        const data     = JSON.parse(body);
+        const worker   = data.workers[config.get('worker')];
+        const hashrate = parseInt(worker.hashrate.replace('H/s', ''), 10);
+
+        console.log(Date.now(), worker.worker, hashrate, threshold);
+        
+        if ( hashrate < threshold ) {
+            exec(command);
+        } else {
+            check();
+        }
+
+    });
+    
+}());
